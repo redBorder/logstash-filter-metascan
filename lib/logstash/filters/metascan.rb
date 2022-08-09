fed6df5520cc5b6b2bce5e73e1d222fec85f2b94
eareyes <eareyes@redborder.com>
2022-08-09 09:59:11 +0100

    Add rescues

--- lib/logstash/filters/metascan.rb
+++ lib/logstash/filters/metascan.rb
@@ -35,10 +35,32 @@ class LogStash::Filters::Metascan < LogStash::Filters::Base
   end # def register
 
   private
+
+  def check_response(response)
+    response_code = response.status
+
+    case response_code
+      when 400
+        response_message = "CODE 400 Bad Request - Unsupported HTTP method or invalid HTTP request (e.g., empty body)"
+      when 401
+        response_message = "CODE 401 Invalid API key - Either missing API key or invalid API is passed."
+      when 403
+        response_message = "CODE 403 Signature lookup limit reached, try again later - The hourly hash lookup limit has been reached for this API key."
+      when 404
+        response_message = "CODE 404 The requested page was not found. Try to upload the file."
+      when 503
+        response_message = "CODE 503 Internal Server Error - Server temporarily unavailable. Try again later."
+      else #when 200
+      response_message = ""
+    end
+
+    [response_code,response_message]
+  end
+
   def get_response_from_hash
     connection = Faraday.new @url_hash
-    score = nil
-    result = nil
+    score = -1
+    result = {}
 
     begin
       response = connection.get @hash do |req|
@@ -47,17 +69,24 @@ class LogStash::Filters::Metascan < LogStash::Filters::Base
         req.options.open_timeout = @timeout
       end
 
-      result = JSON.parse(response.body)
+      response_code,response_message = check_response(response)
+      if response_code != 200
+        @logger.error(response_message)
+        return [score,result]
+      end
 
+      result = JSON.parse(response.body)
       unless result["error"]
         total_avs = result["scan_results"]["total_avs"].to_f
         total_detected_avs = result["scan_results"]["total_detected_avs"].to_f
-
         score = ( (total_detected_avs / total_avs * 100) * @weight ).round
       end
 
     rescue Faraday::TimeoutError
       @logger.error("Timeout trying to contact Metascan")
+
+    rescue Faraday::ConnectionFailed => ex
+      puts ex.message
     end
     [result, score]
   end
@@ -74,6 +103,12 @@ class LogStash::Filters::Metascan < LogStash::Filters::Base
         req.body = "{ \"data\" : #{@path}}"
       end
 
+      response_code,response_message = check_response(response)
+      if response_code != 200
+        @logger.error(response_message)
+        return [score,result]
+      end
+
       data_id = JSON.parse(response.body)["data_id"]
 
     rescue Faraday::TimeoutError
@@ -85,10 +120,11 @@ class LogStash::Filters::Metascan < LogStash::Filters::Base
   def get_response_from_data_id(data_id)
     connection = Faraday.new @url_file + "/"
     progress_percentage = 0
-    result = []
+    score = -1
+    result = {}
+    begin
 
-    while progress_percentage < 100
-      begin
+      while progress_percentage < 100
         response = connection.get data_id do |req|
           req.headers[:apikey] = @apikey
           req.headers["x-file-metadata"] = "1"
@@ -96,13 +132,20 @@ class LogStash::Filters::Metascan < LogStash::Filters::Base
           req.options.open_timeout = @timeout
         end
 
+        response_code,response_message = check_response(response)
+        if response_code != 200
+          @logger.error(response_message)
+          return [score,result]
+        end
+
         result = JSON.parse(response.body)
-
         progress_percentage = result["scan_results"]["progress_percentage"]
-
-      rescue Faraday::TimeoutError
-        @logger.error("Timeout trying to contact Metascan")
       end
+
+    rescue Faraday::TimeoutError
+      @logger.error("Timeout trying to contact Metascan")
+    rescue Faraday::ConnectionFailed => ex
+      puts ex.message
     end
 
     total_avs = result["scan_results"]["total_avs"].to_f
@@ -118,7 +161,12 @@ class LogStash::Filters::Metascan < LogStash::Filters::Base
   def filter(event)
 
     @path = event.get(@file_field)
-    @hash = Digest::MD5.hexdigest File.read @path
+    begin
+      @hash = Digest::MD5.hexdigest File.read @path
+    rescue Errno::ENOENT => ex
+      @logger.error(ex.message)
+    end
+
 
     starting_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
     metascan_result,score = get_response_from_hash
